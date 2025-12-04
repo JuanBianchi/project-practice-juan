0c3e96aa8a1195c3af674ee72ba1a30b0cad56ea
juampibianchi065@gmail.com <juampibianchi065@gmail.com>
2025-12-04 17:37:55 -0300

    code: ✨ introduced aside component with links

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Footer from "./components/Footer";
+import Aside from "./components/Aside";
 
 export default function Home() {
   return (
-    <body>
-      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+        <Aside />
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start mx-auto">
           <Image
             className="dark:invert"
             src="/next.svg"
@@ -64,6 +66,6 @@ export default function Home() {
         </main>
       </div>
       <Footer />
-    </body>
+    </>
   );
 }
